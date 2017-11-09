7a825e90a52e1a182c374eb7087bfc11c270a00b
Isabella <isabella.alstrom@gmail.com>
2017-11-09 20:56:42 +0100

    small changes to flash script

--- python_scripts/flash_light.py
+++ python_scripts/flash_light.py
@@ -3,17 +3,21 @@ entity_id = data.get('entity_id')
 light = hass.states.get(entity_id)
 
 original_state = light.state
+if original_state is 'on':
+    brightness = light.attributes.get('brightness') or 0
+    color_temp = light.attributes.get('color_temp') or 0
+
 if original_state is 'off':
-    hass.services.call('light', 'turn_on', {'entity_id': entity_id})
+    hass.services.call('light', 'turn_on', {'entity_id': entity_id, 'brightness': '255'})
 
 time.sleep(1)
 hass.services.call('light', 'turn_off', {'entity_id': entity_id})
-time.sleep(0.5)
-hass.services.call('light', 'turn_on', {'entity_id': entity_id})
+time.sleep(1)
+hass.services.call('light', 'turn_on', {'entity_id': entity_id, 'brightness': '255'})
 time.sleep(1)
 
 hass.services.call('light', 'turn_off', {'entity_id': entity_id})
-time.sleep(0.5)
+time.sleep(1)
 
 if original_state is 'on':
-    hass.services.call('light', 'turn_on', {'entity_id': entity_id})
+    hass.services.call('light', 'turn_on', {'entity_id': entity_id, 'brightness': brightness})
